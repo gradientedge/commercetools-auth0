c0cacde580f8028700b4abeddc93472429e16ed2
Jimmy Thomson <jimmy@gradientedge.com>
2022-04-01 17:02:41 +0100

    feat: improve logging

--- src/error/CommercetoolsAuth0Error.ts
+++ src/error/CommercetoolsAuth0Error.ts
@@ -1,25 +1,17 @@
 import { CommercetoolsAuth0ErrorCode } from './codes'
+import { ExtendedError, ExtendedErrorOptions } from '@gradientedge/error'
 
-export class CommercetoolsAuth0Error extends Error {
+export class CommercetoolsAuth0Error extends ExtendedError {
   /**
    * Convenience mechanism for identifying that the error that's
    * just been caught is a CommercetoolsAuth0Error.
    */
   public readonly isCommercetoolsAuth0Error = true
-  public code: string | undefined
 
-  constructor(message: string, code?: string) {
-    super(message)
-    this.code = code ?? CommercetoolsAuth0ErrorCode.UNEXPECTED_ERROR
+  constructor(message: string, options?: ExtendedErrorOptions) {
+    super(message, { ...options, code: options?.code ?? CommercetoolsAuth0ErrorCode.UNEXPECTED_ERROR })
 
     // https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#extending-built-ins-like-error-array-and-map-may-no-longer-work
     Object.setPrototypeOf(this, CommercetoolsAuth0Error.prototype)
-  }
-
-  toJSON() {
-    return {
-      isCommercetoolsAuth0Error: true,
-      code: this.code,
-    }
   }
 }
